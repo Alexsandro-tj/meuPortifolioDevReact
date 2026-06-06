a04afde59ec3e88491f3785d74420d0c43ff8a6b
ALEX SANDRO SOUZA DOS SANTOS <alexsandrotj94@gmail.com>
2026-06-06 08:28:34 -0300

    Criação de botao flutuante para a home

--- src/components/Contatos/index.jsx
+++ src/components/Contatos/index.jsx
@@ -2,6 +2,7 @@ import styles from '../Contatos/Contatos.module.css';
 import { FaWhatsapp } from "react-icons/fa";
 
 
+
 export default function Contatos() {
     let enderecoPagina = "http://localhost:5173/";
     return (
@@ -13,21 +14,21 @@ export default function Contatos() {
                 <p>
                     Entre em contato para projetos, parcerias e trabalhos.
                 </p>
-                <a href="https://wa.me/5551991019959"  target="_blank" rel="noopener noreferrer "className={styles.botaoflutuante}>
-                <FaWhatsapp />
-            </a>
-
-            {/* Fomrulário */}
-            <form action="https://formsubmit.co/alexsandrotj94@gmail.com" method="POST">
-                <input type="hidden" name="_next" value={`$enderecoPagina`}></input>
-                <input type="hidden" name="_autoresponse" value="Olá recebemos o seu e-mail, logo entraremos em contato com você. Agradecemos!"></input>
-                {/* <input type="hidden" name="_captcha" value="false" /> */}
-                <input type="text" name="name" required placeholder='Seu nome'></input>
-                <input type="email" name="email" required placeholder='Seu e-mail'></input>
-                <textarea name="message" placeholder='Sua mensagem' id="text-area" required></textarea>
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
+                <a href="https://wa.me/5551991019959" target="_blank" rel="noopener noreferrer " className={styles.botaoflutuante}>
+                    <FaWhatsapp />
+                </a>
+                
+                {/* Fomrulário */}
+                <form action="https://formsubmit.co/alexsandrotj94@gmail.com" method="POST">
+                    <input type="hidden" name="_next" value={`$enderecoPagina`}></input>
+                    <input type="hidden" name="_autoresponse" value="Olá recebemos o seu e-mail, logo entraremos em contato com você. Agradecemos!"></input>
+                    {/* <input type="hidden" name="_captcha" value="false" /> */}
+                    <input type="text" name="name" required placeholder='Seu nome'></input>
+                    <input type="email" name="email" required placeholder='Seu e-mail'></input>
+                    <textarea name="message" placeholder='Sua mensagem' id="text-area" required></textarea>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
 
         </section >
 
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,20 @@
 import styles from '../Footer/Footer.module.css';
+import { FaArrowUp } from "react-icons/fa";
 
 export default function Footer() {
 
     return (
-        
-<footer className={styles.container}>
-    <p>
-        © 2026 Alex Sandro Santos  - Todos os Direitos Reservados
-    </p>
 
-</footer>
+        <footer className={styles.container}>
+            <p>
+                © 2026 Alex Sandro Santos  - Todos os Direitos Reservados
+            </p>
+            <a href="#home" className={styles.arrow}>
+                <FaArrowUp />
+            </a>
+
+
+        </footer>
 
     );
 
