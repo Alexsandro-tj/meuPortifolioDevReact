29cd4d5a373f066e8876de632b90514482d60c75
ALEX SANDRO SOUZA DOS SANTOS <alexsandrotj94@gmail.com>
2026-06-06 10:15:45 -0300

    alteracao nos botoes de scroll up e botao flutuante whatsapp

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,19 +1,18 @@
 import styles from '../Footer/Footer.module.css';
+import {FaWhatsapp} from "react-icons/fa"
 
 
 export default function Footer() {
-
+    let whatsapp = "https://wa.me/5551991019959";
     return (
 
         <footer className={styles.container}>
             <p>
                 © 2026 Alex Sandro Santos  - Todos os Direitos Reservados
             </p>
-            
 
-
+            <a href={whatsapp} target='_blank' rel="noopener noreferrer" className={styles.botaoflutuante}><FaWhatsapp size={50} /></a>
         </footer>
-
     );
 
 }
