6fd4f407e4643d0b2ade86799f577651432d9914
ALEX SANDRO SOUZA DOS SANTOS <alexsandrotj94@gmail.com>
2026-06-07 22:12:53 -0300

    alteracao de responsividade

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,14 +8,15 @@ export default function Header() {
     return (
         <>
             <header id='home' className={style.header}>
-                <a href="#home"><img src={IconHomePage} alt="icone-home-page" className={style.icon} /> </a>
-                <nav>
-                    <a href="#home" className={style.links}>Home</a>
-                    <a href="#sobre" className={style.links}>Sobre</a>
-                    <a href="#projetos" className={style.links}>Projetos</a>
-                    <a href="#contatos" className={style.links}>Contatos</a>
-                    <a href={CV} target="_blank" rel="noopener noreferrer " className={style.links}>Curriculo</a>
-                </nav>
+                    <a href="#home"><img src={IconHomePage} alt="icone-home-page" className={style.icon} /> </a>
+                    <nav>
+                        <a href="#home" className={style.links}>Home</a>
+                        <a href="#sobre" className={style.links}>Sobre</a>
+                        <a href="#projetos" className={style.links}>Projetos</a>
+                        <a href="#contatos" className={style.links}>Contatos</a>
+                        <a href={CV} target="_blank" rel="noopener noreferrer " className={style.links}>Curriculo</a>
+                    </nav>
+                
             </header>
         </>
     );
